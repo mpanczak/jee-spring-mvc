d899982c8274cfb32bf885d40bce6fbc2ffd0004
mpanczak <panczakmateusz@gmail.com>
2023-05-07 19:26:19 +0200

    day4 views, ex 4

--- src/main/java/pl/coderslab/controller/MultiplyController.java
+++ src/main/java/pl/coderslab/controller/MultiplyController.java
@@ -16,7 +16,7 @@ public class MultiplyController {
     public String showMultiplyTable(Model model, @RequestParam(defaultValue= "10") int size) {
 
         List<List<String>> arrayList = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
+
         for (int i = 1; i <= size; i++) {
             List<String> array = new ArrayList<>();
             for (int j = 1; j <= size; j++) {
@@ -24,8 +24,24 @@ public class MultiplyController {
             }
             arrayList.add(array);
         }
-
         model.addAttribute("array", arrayList);
+
         return "multiplyTable";
+    }
+
+    @GetMapping("/paramMultiply/{rows}/{cols}")
+    public String showMultiplyParam(Model model, @PathVariable int rows, @PathVariable int cols) {
+        List<List<String>> arrayList = new ArrayList<>();
+
+        for (int i = 1; i <= rows; i++) {
+            List<String> array = new ArrayList<>();
+            for (int j = 1; j <= cols; j++) {
+                array.add(String.valueOf(i*j));
+            }
+            arrayList.add(array);
+        }
+        model.addAttribute("array", arrayList);
+        model.addAttribute("rows", rows);
+        return "multiplyTableParam";
     }
 }
