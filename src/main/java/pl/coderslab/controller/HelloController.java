f8645da982e7a0621e3f0fa592ac49761d3a60f2
mpanczak <panczakmateusz@gmail.com>
2023-05-07 15:03:17 +0200

    day4 views, ex 1,2

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,8 +1,11 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalTime;
 
 @Controller
 public class HelloController {
@@ -13,5 +16,20 @@ public class HelloController {
     public String hello(){
         return "Hello Kapusta";
     }
+    @GetMapping("/helloView")
+    public String helloView(Model model){
 
+//        LocalTime time = LocalTime.parse("20:20");
+        LocalTime time = LocalTime.now();
+
+        if (time.getHour() >= 8 && time.getHour() < 20){
+            model.addAttribute("textColor", "black");
+            model.addAttribute("backgroundColor", "white");
+        }else{
+            model.addAttribute("textColor", "white");
+            model.addAttribute("backgroundColor", "black");
+        }
+
+        return "home";
+    }
 }
---
+++ src/main/webapp/views/home.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mateusz
+  Date: 07.05.2023
+  Time: 14:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home - CodersLab</title>
+    <style>
+        body {
+            color: ${textColor};
+            background-color: ${backgroundColor};
+        }
+    </style>
+</head>
+<body>
+<h1>Witaj w widoku</h1>
+</body>
+</html>
