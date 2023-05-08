1a3cfba2c515a08b21086c88f16d83b65cacebbe
mpanczak <panczakmateusz@gmail.com>
2023-05-09 00:07:05 +0200

    day4 session, ex 4

--- src/main/java/pl/coderslab/controller/CookieController.java
+++ src/main/java/pl/coderslab/controller/CookieController.java
@@ -26,14 +26,16 @@ public class CookieController {
         response.addCookie(cookie2);
         return "cookies";
     }
-//    @GetMapping("/getcookies")
-//    @ResponseBody
-//    String readCookies(HttpServletRequest request) {
-//        Cookie c1 = WebUtils.getCookie(request, "cookie1");
-//        Cookie c2 = WebUtils.getCookie(request, "cookie2");
-//        return c1.getName() + ": " + c1.getValue().toString() + "<br>" + c2.getName() + ": " + c2.getValue().toString();
-//    }
-    @GetMapping("/getcookies")
+    @GetMapping("/getcookiesUtils")
+    @ResponseBody
+    String readCookies(HttpServletRequest request) {
+        Cookie c1 = WebUtils.getCookie(request, "cookie1");
+        Cookie c2 = WebUtils.getCookie(request, "cookie2");
+        return c1.getName() + ": " + c1.getValue().toString() + "<br>" + c2.getName() + ": " + c2.getValue().toString();
+    }
+
+
+    @GetMapping("/getcookiesAdn")
     @ResponseBody
     String readCookies(@CookieValue("cookie1") String c1, @CookieValue("cookie2") String c2) {
         return "cookie1" + ": " + c1 + "<br>" + "cookie2" + ": " + c2;
